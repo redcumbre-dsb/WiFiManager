b7c9a7331c332be45abe2797fa32989135752a09
Diego Serrano <diego@redcumbre.com>
2021-12-16 09:40:57 -0300

    Cambia _showPassword a true para que la password no se encripte.

--- WiFiManager.h
+++ WiFiManager.h
@@ -476,7 +476,7 @@ class WiFiManager
     int            _staShowStaticFields   = 0;     // ternary 1=always show static ip fields, 0=only if set, -1=never(cannot change ips via web!)
     int            _staShowDns            = 0;     // ternary 1=always show dns, 0=only if set, -1=never(cannot change dns via web!)
     boolean       _removeDuplicateAPs     = true;  // remove dup aps from wifiscan
-    boolean       _showPassword           = false; // show or hide saved password on wifi form, might be a security issue!
+    boolean       _showPassword           = true; // show or hide saved password on wifi form, might be a security issue!
     boolean       _shouldBreakAfterConfig = false; // stop configportal on save failure
     boolean       _configPortalIsBlocking = true;  // configportal enters blocking loop 
     boolean       _enableCaptivePortal    = true;  // enable captive portal redirection
